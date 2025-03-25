0334043980ac4015e016825b6f8611c84725da00
Pedro Mendes <pedrohrmendes@proton.me>
2025-03-25 11:34:59 -0300

    chore: add flakes

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Thesis environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      utils,
+      nixpkgs,
+      ...
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              python313
+              uv
+            ];
+
+            shellHook = ''
+              VENV="./.venv/bin/activate"
+
+              if [[ ! -f $VENV ]]; then
+                ${pkgs.uv}/bin/uv venv
+              fi
+
+              source "$VENV"
+            '';
+
+            LD_LIBRARY_PATH = lib.makeLibraryPath [
+              stdenv.cc.cc.lib
+              zlib
+            ];
+          };
+      }
+    );
+}
