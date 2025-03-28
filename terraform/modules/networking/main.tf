549179d75bfe0ab13d370d055ac34f01fc0341cd
Pedro Mendes <pedrohrmendes@proton.me>
2025-03-28 12:11:48 -0300

    feat: add router

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -1,3 +1,36 @@
+resource "google_compute_firewall" "allow_ssh_gke" {
+  name    = "allow-ssh"
+  network = google_compute_network.poc.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["35.235.240.0/20"]
+}
+
+resource "google_compute_router" "poc" {
+  name    = "poc"
+  network = google_compute_network.poc.name
+  region  = var.region
+
+}
+
+resource "google_compute_router_nat" "poc" {
+  name                               = "gke-datalake-router-nat"
+  router                             = google_compute_router.poc.name
+  region                             = google_compute_router.poc.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = google_compute_subnetwork.poc.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+  log_config {
+    enable = true
+    filter = "ALL"
+  }
+}
+
 resource "google_compute_network" "poc" {
   name                    = "poc"
   auto_create_subnetworks = false
