81000dfa80ba98b87ede7eae6c03103fd45e611e
Pedro Mendes <pedrohrmendes@proton.me>
2025-04-08 23:31:18 -0300

    refactor: remove unused variables

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,8 +1,6 @@
 locals {
   region                  = data.infisical_secrets.gke.secrets["REGION"].value
   project_id              = data.infisical_secrets.gke.secrets["PROJECT_ID"].value
-  ssh_public_key          = data.infisical_secrets.gke.secrets["VM_SSH_PUBLIC_KEY"].value
-  ssh_private_key         = data.infisical_secrets.gke.secrets["VM_SSH_PRIVATE_KEY"].value
   host                    = data.infisical_secrets.gke.secrets["HOST"].value
   cluster_service_account = data.infisical_secrets.gke.secrets["CLUSTER_SERVICE_ACCOUNT"].value
   machine_type = {
--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,8 @@ module "gke" {
 
   project_id              = local.project_id
   region                  = local.region
-  vpc_network             = module.networking.vpc_network_name
-  vpc_subnetwork          = module.networking.vpc_subnetwork_name
+  vpc_network             = module.networking.vpc_network
+  vpc_subnetwork          = module.networking.vpc_subnetwork
   machine_type            = local.machine_type
   disk_size               = local.disk_size
   host                    = local.host
@@ -22,12 +22,10 @@ module "gke" {
 module "compute" {
   source = "./modules/compute"
 
-  project_id      = local.project_id
-  region          = local.region
-  vpc_network     = module.networking.vpc_network_name
-  vpc_subnetwork  = module.networking.vpc_subnetwork_name
-  machine_type    = local.machine_type.default
-  disk_size       = local.disk_size.large
-  ssh_public_key  = local.ssh_public_key
-  ssh_private_key = local.ssh_private_key
+  project_id     = local.project_id
+  region         = local.region
+  vpc_network    = module.networking.vpc_network
+  vpc_subnetwork = module.networking.vpc_subnetwork
+  machine_type   = local.machine_type.default
+  disk_size      = local.disk_size.large
 }
