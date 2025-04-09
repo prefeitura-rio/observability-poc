8c2e53092c2f44a8960600eb1991da1f20b7f1be
Pedro Mendes <pedrohrmendes@proton.me>
2025-04-09 00:04:26 -0300

    fix: update scripts

--- terraform/modules/compute/scripts/init_vm.sh
+++ terraform/modules/compute/scripts/init_vm.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 ENDPOINT="https://raw.githubusercontent.com/prefeitura-rio/observability-poc/refs/heads/master/terraform/modules/compute"
-
+UBUNTU_CODENAME=jammy
+MAIN_DIR="/poc"
 FILES=(
     "ansible/playbook.yaml"
     "ansible/requirements.yaml"
@@ -9,19 +10,20 @@ FILES=(
     "docker/traefik.yaml"
 )
 
-mkdir -p "$HOME"/poc
-
-sudo apt-get update
-
-sudo apt-get install -y python3 python3-pip
-
-sudo pip3 install ansible
+sudo mkdir -p "$MAIN_DIR"
 
 for file in "${FILES[@]}"; do
     dest=$(basename "$file")
-    curl -o "$HOME/poc/$dest" "$ENDPOINT/$file"
+    curl -o "$MAIN_DIR/$dest" "$ENDPOINT/$file"
 done
 
-ansible-galaxy install -r "$HOME"/poc/requirements.yaml
 
-ansible-playbook "$HOME"/poc/playbook.yaml
+wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
+
+sudo apt update && sudo apt install ansible
+
+ansible-galaxy install -r "$MAIN_DIR"/requirements.yaml
+
+ansible-playbook "$MAIN_DIR"/playbook.yaml
